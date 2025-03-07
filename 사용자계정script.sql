ef86ee8ff85fa98ca93bbab86c50f750a38ad71c
leehoyeun <dlghdus100@gmail.com>
2025-03-07 17:02:18 +0900

    first commit

---
+++ 사용자계정script.sql
@@ -0,0 +1,25 @@
+--한줄 주석
+
+/*
+ * 범위주석
+ * 
+ * 
+ * */
+-- 선택한 SQL 수행 : 내가 수행하고자 하는 구문에 커서를두고 CRTL + ENTER
+-- 전체 SQL 수행 : 전체 구문을 활성화 시킨 채로 alt + x 
+
+--12c 버전 이전 문법 허용 해주는 구문
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+--계정을 생성 구문 (username : kh / password: kh1234)
+CREATE USER kh IDENTIFIED BY kh1234;
+
+
+GRANT RESOURCE, CONNECT TO kh;
+-- 사용자 계정 권한 부여 설정
+-- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
+-- CONNECT : DB에 연결하고 로그인 할 수 있는 권한
+
+
+ALTER USER kh DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
+--객체가 생성될 수 있는 공간 할당량을 무제한 지정
