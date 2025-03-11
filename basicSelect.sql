7326130499282701d011c4c9c8819951e6a82862
leehoyeun <dlghdus100@gmail.com>
2025-03-12 08:41:14 +0900

    베이직

--- basicSelect.sql
+++ basicSelect.sql
@@ -4,17 +4,74 @@ SELECT * FROM TB_DEPARTMENT;
 SELECT * FROM TB_GRADE;
 SELECT * FROM TB_PROFESSOR;
 SELECT * FROM TB_STUDENT;
-
+--1.
 SELECT DEPARTMENT_NAME "학과 명" , CATEGORY"계열"
 FROM TB_DEPARTMENT ;
-
-SELECT DEPARTMENT_NAME"의 정원은", LENGTH(DEPARTMENT_NAME)"명 입니다"
+--2.
+SELECT DEPARTMENT_NAME||'의 정원은'||CAPACITY||'명입니다'AS"학과별 정원"
 FROM TB_DEPARTMENT;
 
 
+--3.
 SELECT STUDENT_NAME
-FROM  TB_STUDENT ts ;
-WHERE EMP_
+FROM  TB_STUDENT  
+WHERE SUBSTR(STUDENT_SSN,8,1)='2'--여자
+AND ABSENCE_YN ='Y'
+AND DEPARTMENT_NO ='001';
+
+--4.도서관에서 대출 도서 장기 연체자 들을 찾아 이름을 게시하고자 한다.
+--그 대상자들의 학번이 다음과 같을 때 대상자들을 찾는 적절한 SQL문을 작성하시요.
+SELECT * FROM TB_STUDENT;
+
+SELECT STUDENT_NAME
+FROM TB_STUDENT
+WHERE STUDENT_NO IN ('A513079','A513090','A513091','A513110','A513119')
+ORDER BY STUDENT_NAME DESC ;
 
 
+--5. 입학 정원이 20명 이상 30명 이하인 학과들의 학과 이름과 계열을 출력하시오.
+SELECT DEPARTMENT_NAME ,CATEGORY
+FROM TB_DEPARTMENT
+WHERE CAPACITY >= 20 AND CAPACITY <= 30;
+--WHERE CAPACITY BETWEEN 20 AND 30 ; 
+
+--6.춘 기술대학교는 총장의 제외하고 모든 교수들이 소속 학과를 가지고 있다.
+--그럼 춘 기술대학교 총장의 이름을 알아낼 수 있는 SQL 문장을 작성하시오.
+
+SELECT *  FROM TB_PROFESSOR tp ;
+
+SELECT PROFESSOR_NAME
+FROM TB_PROFESSOR 
+--WHERE DEPARTMENT_NO IS NULL;
+WHERE DEPARTMENT_NO IS NULL;
+--WHERE DEPARTMENT_NO NOT IN (value1, value2, ...) ;
+
+--7.혹시 전산상의 착오로 학과가 지정되어 있지 않은 학생이 있는지 확인하고자 한다.
+--어떠한 SQL 문장을 사용하면 될 것인지 작성하시오
+
+SELECT STUDENT_NAME
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO IS NULL;
+--개열받네
+ 
+SELECT *  FROM TB_CLASS;
+
+--8.수강신청을 하려고 한다 . 선수과목 여부를 혹인해야 하는데 , 선수과목이
+--조재하는 과목들은 어떤 과목인지 과목번호를 조회해보시오.
+SELECT PREATTENDING_CLASS_NO
+FROM TB_CLASS 
+WHERE PREATTENDING_CLASS_NO IS NOT NULL;
+
+--9.춘 대학에는 어떤 계열(CATEGORY)들이 있는지 조회해보시오.
+SELECT DISTINCT CATEGORY
+FROM TB_DEPARTMENT;
+
+SELECT * FROM TB_STUDENT;
+--10 . 02학번 전주 거주자들의 모임을 만들려고한다.휴학한 사람들은 제외한 재학중인
+        --학생들의 학번 ,이름 , 주민번호를 출력하는 구문을 작성하시오.
+SELECT STUDENT_NO , STUDENT_NAME ,STUDENT_SSN
+FROM TB_STUDENT 
+WHERE SUBSTR (STUDENT_NO,1,2)='A2'AND 
+SUBSTR (STUDENT_ADDRESS,1,2)='전주'AND 
+ABSENCE_YN = 'N'; 
 
